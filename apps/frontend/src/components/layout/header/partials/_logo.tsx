587d6f9ffdfee593fc737a08dd9e3f26318ad63e
Shannon Gray <shannon.gray@episerver.com>
2025-07-08 17:02:39 -0700

    Update _logo.tsx

--- apps/frontend/src/components/layout/header/partials/_logo.tsx
+++ apps/frontend/src/components/layout/header/partials/_logo.tsx
@@ -17,11 +17,12 @@ export const Logo: FunctionComponent<LogoProps> = ({
   const defaultValue = useMemo(() => { return { logo, theme_switcher: false } }, [ logo ])
 
   // Get the logo configuration
+  const myLogoUrl="https://www.google.com/url?sa=i&url=https%3A%2F%2Fmaverik.com%2F&psig=AOvVaw1In6kEfxZLpYb9ydu4_5gk&ust=1752105396766000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIDD5sK7ro4DFQAAAAAdAAAAABAK";
   const { logo: logoUrl } = useFlag("layout_configuration", defaultValue);
   return (
     <Link href="/" className="flex items-center grow-0 shrink-0" {...divProps}>
       <Image
-        src={logoUrl}
+        src={myLogoUrl}
         alt="Mosey Bank Logo"
         fill
         unoptimized
